cfc123189fd2807047ad2e7ffc1233ecdae8c776
dimo4ka-cmd <dimaprovihor@gmail.com>
2025-05-18 14:58:34 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,13 @@
 import logging
 
 BOT_TOKEN = "7568460242:AAEoecvAVSPgLP-wJCJJXgfmoNsPcuAdWXA"
+PAYOUT_BOT_USERNAME = "@hated2testbot"
 ADMIN_IDS = [1828106357]
 LOG_FILE = "bot.log"
-RATE_LIMIT_SECONDS = 300
 DB_FILE = "users.db"
+REFERRAL_CODE = "аренда11"
+QUEUE_LIMIT = 3
+NOTIFY_POSITION = 3
 
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
