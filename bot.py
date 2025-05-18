237d993ae2c69092872b2de2db56eba550b2ce51
dimo4ka-cmd <dimaprovihor@gmail.com>
2025-05-18 15:33:01 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,6 +15,7 @@ def main():
     app.add_handler(CallbackQueryHandler(button_handler))
     app.add_error_handler(error_handler)
 
+    logger.info("Starting main bot")
     app.run_polling()
 
 if __name__ == "__main__":
